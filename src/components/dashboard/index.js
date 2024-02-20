7e56ac3f14be59ad5f614554311ecf17e9d9007f
unknown <muak1ayomide@gmail.com>
2024-02-20 02:18:56 +0100

    testing

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,6 +1,6 @@
 const Dashboard = () => {
   return (
-    <div className="flex-[0.7] w-full h-full border-green-500 border p-4">
+    <div className="flex-[0.8] w-full h-full border-green-500 border p-4">
       Dashboard
     </div>
   );
