37e2cbd4ccb82eb7bc3169b0affe81ca59ffb15e
muakone <muak1ayomide@gmail.com>
2024-02-22 16:15:53 +0100

    made changes to the progress bar

--- src/components/dashboard/visual-cards.jsx
+++ src/components/dashboard/visual-cards.jsx
@@ -1,54 +1,89 @@
 import { Flex, ProgressCircle } from "@tremor/react";
 import { Settings } from "lucide-react";
 
-export const Efficiency = () => {
+export const Voltage = () => {
   return (
     <>
       <div className="flex justify-between text-xs font-medium items-center">
-        <p>Energy Efficency</p>
+        <p>Mains Voltage</p>
         <Settings size={12} />
       </div>
 
       <div className="flex items-center justify-center gap-3 w-full text-xs mt-2 opacity-50">
-        <p className="">Highest 83% </p>
-        <p>Lowest 27%</p>
+        <p className="">Highest 270V </p>
+        <p>Lowest 0V</p>
       </div>
 
       <Flex className="space-x-5" justifyContent="center">
-        <ProgressCircle value={75} size="xl" strokeWidth={8} color="violet">
-          <span className="text-base font-bold text-slate-700">75%</span>
+        <ProgressCircle
+          value={0.37 * 190}
+          size="xl"
+          strokeWidth={8}
+          color="violet"
+        >
+          <span className="text-base font-bold text-slate-700">190V</span>
         </ProgressCircle>
       </Flex>
 
-      <p className="text-xs text-center opacity-50">
-        Based on the past 1 hour usage
-      </p>
+      <p className="text-xs text-center opacity-50">Based on Live Feed</p>
     </>
   );
 };
 
-export const UsageLoad = () => {
+export const Current = () => {
   return (
     <>
       <div className="flex justify-between text-xs font-medium items-center">
-        <p>Live Usage Load</p>
+        <p>Load Current</p>
         <Settings size={12} />
       </div>
 
       <div className="flex items-center justify-center gap-3 w-full text-xs mt-2 opacity-50">
-        <p className="">Highest 83% </p>
-        <p>Lowest 27%</p>
+        <p className="">Highest 1000A </p>
+        <p>Lowest 0A</p>
       </div>
 
       <Flex className="space-x-5" justifyContent="center">
-        <ProgressCircle value={65} size="xl" strokeWidth={8} color="violet">
-          <span className="text-sm font-bold text-slate-700">376 kw/h</span>
+        <ProgressCircle
+          value={0.1 * 400}
+          size="xl"
+          strokeWidth={8}
+          color="violet"
+        >
+          <span className="text-sm font-bold text-slate-700">400A</span>
         </ProgressCircle>
       </Flex>
 
-      <p className="text-xs text-center opacity-50">
-        Based on the past 1 hour usage
-      </p>
+      <p className="text-xs text-center opacity-50">Based on Live Feed</p>
+    </>
+  );
+};
+
+export const PowerFactor = () => {
+  return (
+    <>
+      <div className="flex justify-between text-xs font-medium items-center">
+        <p>Power factor</p>
+        <Settings size={12} />
+      </div>
+
+      <div className="flex items-center justify-center gap-3 w-full text-xs mt-2 opacity-50">
+        <p className="">Highest 1 </p>
+        <p>Lowest 0</p>
+      </div>
+
+      <Flex className="space-x-5" justifyContent="center">
+        <ProgressCircle
+          value={0.4 * 100}
+          size="xl"
+          strokeWidth={8}
+          color="violet"
+        >
+          <span className="text-sm font-bold text-slate-700">0.4</span>
+        </ProgressCircle>
+      </Flex>
+
+      <p className="text-xs text-center opacity-50">Based on Live Feed</p>
     </>
   );
 };
--- src/components/dashboard/visuals.jsx
+++ src/components/dashboard/visuals.jsx
@@ -4,7 +4,11 @@ import { Card } from "../ui/card";
 import Chart from "@/components/dashboard/area-chart";
 import { Settings } from "lucide-react";
 import { ProgressCircle } from "@tremor/react";
-import { Efficiency, UsageLoad } from "@/components/dashboard/visual-cards";
+import {
+  Current,
+  PowerFactor,
+  Voltage,
+} from "@/components/dashboard/visual-cards";
 
 const Visuals = () => {
   return (
@@ -17,11 +21,14 @@ const Visuals = () => {
       <div className="flex flex-col sm:flex-row gap-3 items-center justify-between w-full p-3">
         {/* Card2 */}
         <div className="sm:flex-[0.5] w-full h-auto border rounded-md p-3 space-y-5">
-          <UsageLoad />
+          <Voltage />
+        </div>
+        <div className="sm:flex-[0.5] w-full h-auto border rounded-md p-3 space-y-5">
+          <PowerFactor />
         </div>
         {/* Card3 */}
         <div className="sm:flex-[0.5] w-full h-auto border rounded-md p-3 space-y-5">
-          <Efficiency />
+          <Current />
         </div>
       </div>
     </div>
